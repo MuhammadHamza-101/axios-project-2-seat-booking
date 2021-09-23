33a0754a801a02910de563798416258a5a9e216c
Muhammad-Hamza <hamzabawani22@gmail.com>
2021-09-24 01:45:25 +0500

    app.js
    work done

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 //starting js to get values....
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.seat.row:not(.Occupied)');
-const count = document.getElementById('count')
-const total = document.getElementById('total')
-const movieSelect = document.getElementById('movie')
+const seats = document.querySelectorAll('.seat.row:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+let movieSelect = document.getElementById('movie');
 
-const moviePrice = movieSelect.value;
-console.log(moviePrice);
+let ticketPrice = +movieSelect.value;
+
+
+
+function updateSelectedCounts(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    const selectedSeatsCount = selectedSeats.length
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
+    
+}; 
+
+container.addEventListener('click', e =>{
+    if(e.target.classList.contains('seat')&&
+    !e.target.classList.contains('occupied')
+    ){
+       e.target.classList.toggle('selected');
+       updateSelectedCounts();
+    }
+});
+
+movieSelect.addEventListener('change',e =>{  
+    ticketPrice = +e.target.value
+    updateSelectedCounts()
+})
